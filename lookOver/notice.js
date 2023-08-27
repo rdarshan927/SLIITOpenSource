a9c258d555bbcdad60d1027aa76b4c217bc7a21b
rdarshan927 <rdarshan927@gmail.com>
2023-08-28 00:19:32 +0530

    targets updated in js

---
+++ lookOver/notice.js
@@ -0,0 +1,18 @@
+// right content
+
+function notice(text){
+    if(text == "type-r1"){
+        document.getElementById('type-r-in').innerHTML = "link to projects we have done";
+        //document.getElementById('type-r-in-2').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, ex. Sint sunt quam dignissimos facere quas iure reiciendis id accusamus, assumenda, illum iusto ipsa ut provident nesciunt tenetur cumque officiis.";
+    }
+
+    else if(text == "type-r2"){
+        document.getElementById('type-r-in-2').innerHTML = "any-link-for-our-batch-money-storing";
+    }
+    else if(text == "type-r3"){
+        document.getElementById('type-r-in-2').innerHTML = "torequest-projects-to-us";
+    }
+    else if(text == "type-r4"){
+        document.getElementById('type-r-in-2').innerHTML = "for-visitors-sharing-the-page";
+    }
+}
